c180bad960110447ea86d777fd2b115479656a7d
ztroze <rortizcompsci@gmail.com>
2023-08-31 01:20:49 -0500

    Upload video after authentication

---
+++ yt-api-service/functions/src/index.ts
@@ -0,0 +1,52 @@
+import * as functions from "firebase-functions";
+import * as logger from "firebase-functions/logger";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
+initializeApp();
+
+const firestore = new Firestore();
+const storage = new Storage();
+
+const rawVideoBucketName = "pkmn-yt-raw-videos";
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
+});
+
+// https://cloud.google.com/storage/docs/samples/storage-generate-upload-signed-url-v4#storage_generate_upload_signed_url_v4-nodejs
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // Check if the user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated."
+    );
+  }
+
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
+});
